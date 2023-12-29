c687ee95025efbbace1527b8e58bbf8f1434a68e
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2023-12-30 02:03:30 +0300

    Fix styles and add more samples

--- main.go
+++ main.go
@@ -12,12 +12,26 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+const (
+	maxLeaps = 50
+	pageSize = 10
+)
+
 type Leap struct {
-	Name     string
-	Original string
-	Tags     []string
-	Shortcut string
+	Name        string
+	Original    string
+	Tags        []string
+	Shortcut    string
+	Description string
 }
+
+type LeapList struct {
+	Leaps []Leap
+	Size  int
+	Next  int
+}
+
+var samples = sampleLeaps(maxLeaps)
 
 type TemplateRenderer struct {
 	templates *template.Template
@@ -27,11 +41,26 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-var tlds = []string{"com", "net", "org", "gov", "edu", "info", "biz", "co", "io", "pro"}
-var lorem = []string{
-	"animi", "voluptatem", "repellendusg", "laboriosam", "facilis",
-	"assumenda", "autem", "vero", "veniam", "repudiandae", "repellat", "corporis",
-	"ullam", "temporibus", "incidunt", "harum", "nulla", "Animi", "praesentium", "quasi", "ipsam",
+// A custom context that extends `echo.Context`
+type CustomContext struct {
+	echo.Context
+}
+
+//	Attempts to get the url query parameter `s` and convert it to an int.
+//
+// Defaults to `n` or zero is if the parameter is empty or cannot be converted to int.
+func (c *CustomContext) IntParam(s string, n ...int) int {
+	if len(n) == 0 {
+		n = []int{0}
+	}
+	p := c.QueryParam(s)
+	num, err := strconv.Atoi(p)
+	if err != nil || num == 0 {
+		return n[0]
+	}
+	fmt.Println("**** n = ", n[0])
+	fmt.Println("**** num = ", num)
+	return num
 }
 
 // selects a string from a given slice of strings `choices`
@@ -41,34 +70,36 @@ func randomSelect(choices []string) string {
 }
 
 func randomUrl() string {
-	return fmt.Sprintf("https://%v.%v/%v", randomSelect(lorem), randomSelect(tlds), randomSelect(lorem))
+	return fmt.Sprintf("https://%v.%v/%v", randomSelect(nouns), randomSelect(tlds), randomSelect(nouns))
 }
 
 // provides a slice of `Leap` for testing
 func sampleLeaps(n int) map[string]Leap {
-
 	randTagsN := func(j int) []string {
 		var tags []string
 		for i := 0; i < j; i++ {
-			tags = append(tags, randomSelect(lorem))
+			tags = append(tags, randomSelect(adjectives))
 		}
 		return tags
 	}
 
 	l := make(map[string]Leap)
 	for i := 0; i < n; i++ {
-		sc := randomSelect(lorem)
+		sc := randomSelect(nouns)
+		_, set := l[sc]
+		if set {
+			sc = fmt.Sprintf("%v%v", sc, i)
+		}
 		l[sc] = Leap{
-			Name:     randomSelect(lorem),
-			Shortcut: sc,
-			Original: randomUrl(),
-			Tags:     randTagsN(rand.Intn(6)),
+			Name:        randomSelect(nouns),
+			Shortcut:    sc,
+			Original:    randomUrl(),
+			Tags:        randTagsN(rand.Intn(6)),
+			Description: randomSelect(descriptions),
 		}
 	}
 	return l
 }
-
-var samples = sampleLeaps(10)
 
 func nLeaps(n int) []Leap {
 	l := make([]Leap, n)
@@ -98,6 +129,12 @@ func leapsByTag(tag string) []Leap {
 
 func main() {
 	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c}
+			return next(cc)
+		}
+	})
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -110,29 +147,30 @@ func main() {
 	e.Static("/assets", "assets")
 
 	e.GET("/", func(c echo.Context) error {
-		n := c.QueryParam("samples")
-		if n == "" {
-			n = "5"
-		}
-		num, _ := strconv.Atoi(n)
+		cc := c.(*CustomContext)
+		num := cc.IntParam("samples", 10)
 		return c.Render(http.StatusOK, "home.html", num)
 	})
-
 	e.GET("/leaps", func(c echo.Context) error {
-		n := c.QueryParam("n")
-		if n == "" {
-			n = "5"
-		}
-		num, _ := strconv.Atoi(n)
+		cc := c.(*CustomContext)
+		num := cc.IntParam("n", 10)
+		fmt.Println("***** leap n = ", num)
 		leaps := nLeaps(num)
-		fmt.Println("** samples", len(leaps))
-		return c.Render(http.StatusOK, "leaps.html", leaps)
+		list := LeapList{
+			Leaps: leaps,
+			Size:  len(leaps),
+			Next:  2,
+		}
+		return c.Render(http.StatusOK, "leaps.html", list)
 	})
 	e.GET("/:sc", func(c echo.Context) error {
 		sc := c.Param("sc")
-		return c.Redirect(http.StatusTemporaryRedirect, samples[sc].Original)
+		sample, set := samples[sc]
+		if !set {
+			return c.Render(http.StatusNotFound, "404.html", nil)
+		}
+		return c.Redirect(http.StatusTemporaryRedirect, sample.Original)
 	})
-
 	e.GET("/tag/:t", func(c echo.Context) error {
 		tag := c.Param("t")
 		tagLeaps := leapsByTag(tag)
@@ -145,15 +183,17 @@ func main() {
 		return c.Render(http.StatusInternalServerError, "500.html", nil)
 	})
 
-	e.GET("/api/leaps", func(c echo.Context) error {
+	api := e.Group("/api")
+	api.GET("/leaps", func(c echo.Context) error {
 		n := c.QueryParam("n")
 		if n == "" {
-			n = "5"
+			n = "10"
 		}
 		num, _ := strconv.Atoi(n)
-
-		leaps := sampleLeaps(num)
-		fmt.Println("** samples", len(leaps))
+		if num > maxLeaps {
+			return c.JSON(http.StatusBadRequest, fmt.Sprintf("Too many leaps requested, max is %v", maxLeaps))
+		}
+		leaps := nLeaps(num)
 		return c.JSON(http.StatusOK, leaps)
 	})
 
